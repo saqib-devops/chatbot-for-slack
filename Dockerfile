6975d86a302c7c689b0a991bf28f5e4c9848f123
saqib-devops <saqibahmad778866@gmail.com>
2023-04-04 04:30:08 -0400

    ChatBot created

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.9
+
+WORKDIR /app
+RUN curl https://sh.rustup.rs -sSf | bash -s -- -y
+ENV PATH="/root/.cargo/bin:${PATH}"
+COPY requirements.txt /app
+
+RUN pip install -r requirements.txt
+
+COPY app.py /app
+
+ENTRYPOINT ["python"]
+CMD ["app.py"]
---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+import re
+import time
+from threading import Thread
+
+from revChatGPT.V3 import Chatbot
+from slack_bolt import App
+
+ChatGPTConfig = {
+    "api_key": os.getenv("OPENAI_API_KEY"),
+}
+
+if os.getenv("OPENAI_ENGINE"):
+    ChatGPTConfig["engine"] = os.getenv("OPENAI_ENGINE")
+
+app = App()
+chatbot = Chatbot(**ChatGPTConfig)
+
+
+@app.event("app_mention")
+def event_test(event, say):
+    prompt = re.sub('\\s<@[^, ]*|^<@[^, ]*', '', event['text'])
+    try:
+        response = chatbot.ask(prompt)
+        user = event['user']
+        send = f"<@{user}> {response}"
+    except Exception as e:
+        print(e)
+        send = "We're experiencing exceptionally high demand. Please, try again."
+
+    # Get the `ts` value of the original message
+    original_message_ts = event["ts"]
+
+    # Use the `app.event` method to send a reply to the message thread
+    say(send, thread_ts=original_message_ts)
+
+@app.event("message")
+def event_test(event, say):
+    prompt = re.sub('\\s<@[^, ]*|^<@[^, ]*', '', event['text'])
+    try:
+        response = chatbot.ask(prompt)
+        user = event['user']
+        send = response
+    except Exception as e:
+        print(e)
+        send = "We're experiencing exceptionally high demand. Please, try again."
+
+    # reply message to new message
+    say(send)
+
+def chatgpt_refresh():
+    while True:
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    thread = Thread(target=chatgpt_refresh)
+    thread.start()
+    app.start(3000)  # POST http://localhost:4000/slack/events
